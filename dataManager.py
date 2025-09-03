3a812b2632094a7b716c5697ba1395b8cacacde0
Lewis Milun <lewismilun@gmail.com>
2025-09-03 10:46:54 -0400

    Commented dataManager

--- dataManager.py
+++ dataManager.py
@@ -5,11 +5,17 @@ headers = ['Type', 'Year', 'player_yead_id', 'Name', 'Age', 'Team(s)', 'pos', 'G
 # Import csv to a pandas data frame
 rawData = pd.read_csv("rawData.csv", header = 0, names = headers, low_memory=False)
 
+
+#Beginning process of cleaning data by finding all duplicate names
 playerYears = {}
 
+#Iterates through all years post 2000
 for i in rawData[rawData['Year'] >= 2000].values:
+    #Adds this player's season to playerYears
     if i[3] in playerYears.keys():
+        #Rushing and receiving are separate
         if i[0] == 'Rushing':
+            #In the form of name: [year, type, touches]
             playerYears[i[3]].append([i[1], i[0], i[9]])
         else:
             playerYears[i[3]].append([i[1], i[0], i[10]])
@@ -20,7 +26,10 @@ for i in rawData[rawData['Year'] >= 2000].values:
             playerYears[i[3]] = [[i[1], i[0], i[10]]]
 
 toPop = []
+
+#Removes players who never meet minimum seasons
 for i in playerYears.keys():
+    #Makes dictionary of all years for this player
     this = {}
     for j in playerYears[i]:
         if j[0] in this.keys():
@@ -29,14 +38,19 @@ for i in playerYears.keys():
             this[j[0]] = [0,j[2]]
         if j[1] == 'Rushing':
             this[j[0]][0] += j[2]
+
+    #Checks if minimum requirements of 50 carries and 75 touches is met for each season
     good = False
     for j in this.keys():
         if this[j][0] > 50 and this[j][1] > 75:
             good = True
+    
+    #If minimums aren't met, player name is added to list of players to remove
     if good == False:
         toPop.append(i)
 
+#Removes all players who don't ever meet minimum
 for i in toPop:
     playerYears.pop(i)
 
-print(playerYears)
+#Now we are left with all players who at some point meet the minimum
