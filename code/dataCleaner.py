773416928d514271c04c5ddf4e5b394b254d70f4
Lewis Milun <lewismilun@gmail.com>
2025-09-10 11:30:15 -0400

    Filtered out all players who never fit criteria

--- code/dataCleaner.py
+++ code/dataCleaner.py
@@ -3,23 +3,10 @@ import pandas as pd
 eligiblePlayers = pd.read_csv('data/rawPlayerData.csv')
 allSeasons = pd.read_csv('data/rawSeasonsData.csv')
 
-potentiallyEligibleSeasons = pd.DataFrame(columns = allSeasons.columns)
+eligibleSeasons = pd.DataFrame(columns = allSeasons.columns)
 
 for _,a in allSeasons.iterrows():
-    if a['Player'] in eligiblePlayers['Player'].values:
-        potentiallyEligibleSeasons.loc[len(potentiallyEligibleSeasons)] = a
+    if a['playerID'] in eligiblePlayers['playerID'].values:
+        eligibleSeasons.loc[len(eligibleSeasons)] = a
 
-playerCounts = {}
-
-for _,a in potentiallyEligibleSeasons.iterrows():
-    if a['Player'] not in playerCounts.keys():
-        playerCounts[a['Player']] = 1
-    else:
-        playerCounts[a['Player']] += 1
-
-print(playerCounts)
-
-for a in playerCounts.keys():
-    for _,b in eligiblePlayers.iterrows():
-        if a == b['Player'] and playerCounts[a] != b['Count']:
-            pass
+print(eligibleSeasons)
