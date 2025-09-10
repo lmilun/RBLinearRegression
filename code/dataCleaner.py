2db5e310150449920adc11ab3a1cc66aeabf52ff
Lewis Milun <lewismilun@gmail.com>
2025-09-10 11:55:13 -0400

    Began making yptTimeline

--- code/dataCleaner.py
+++ code/dataCleaner.py
@@ -10,3 +10,19 @@ for _,a in allSeasons.iterrows():
         eligibleSeasons.loc[len(eligibleSeasons)] = a
 
 eligibleSeasons.to_csv('data/playerSeasons.csv')
+
+
+cols = ['playerID', 'Player']
+
+for i in range(20,40):
+    cols.append(i)
+
+print(cols)
+
+yptTimeline = pd.DataFrame(columns = cols)\
+
+for _,a in eligiblePlayers.iterrows():
+    new_row = pd.DataFrame({'playerID': [a['playerID']], 'Player': [a['Player']]})
+    yptTimeline = pd.concat([yptTimeline, new_row], ignore_index=True)
+
+print(yptTimeline)
