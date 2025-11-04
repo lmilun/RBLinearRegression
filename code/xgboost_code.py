d21129d6fb72cc3d5465485971d084299b8724eb
Lewis Milun <lewismilun@gmail.com>
2025-11-04 13:37:57 -0500

    Changed to randomly select parameters for speed

--- code/xgboost_code.py
+++ code/xgboost_code.py
@@ -27,47 +27,51 @@ cols = ['n_estimators','learning_rate', 'max_depth','subsample','colsample_bytre
 tuning = pd.DataFrame(columns = cols)
 
 
-for n_estimators in range(100,1000,1):
-    for learning_rate in range(1,30,1):
-        for max_depth in range(3,10,1):
-            for subsample in range(50,100,5):
-                for colsample_bytree in range(50,100,5):
-                    model = XGBRegressor(
-                        n_estimators=n_estimators,
-                        learning_rate=learning_rate / 100,
-                        max_depth=max_depth,
-                        subsample=subsample / 100,
-                        colsample_bytree=colsample_bytree / 100,
-                        random_state=42,
-                        tree_method="hist"
-                    )
+for i in range(250):
+    n_estimators= np.random.randint(100,1000)
+    learning_rate=np.random.randint(1,30) / 100
+    max_depth=np.random.randint(3,10)
+    subsample=np.random.randint(50,100) / 100
+    colsample_bytree=np.random.randint(50,100) / 100
 
-                    cv = KFold(n_splits = 5, shuffle = True, random_state = 42)
+    model = XGBRegressor(
+        n_estimators=n_estimators,
+        learning_rate=learning_rate / 100,
+        max_depth=max_depth,
+        subsample=subsample / 100,
+        colsample_bytree=colsample_bytree / 100,
+        random_state=42,
+        tree_method="hist"
+    )
 
-                    cv_scores = cross_val_score(model, X, Y, cv=cv, scoring = None)
+    cv = KFold(n_splits = 5, shuffle = True, random_state = 42)
 
-                    new_row = pd.DataFrame(columns = cols)
+    cv_scores = cross_val_score(model, X, Y, cv=cv, scoring = None)
 
-                    new_row['n_estimators'] = n_estimators
-                    new_row['learning_rate'] = learning_rate
-                    new_row['max_depth'] = max_depth
-                    new_row['subsample'] = subsample
-                    new_row['colsample_bytree'] = colsample_bytree
-                    new_row['cv_score1'] = cv_scores[0]
-                    new_row['cv_score2'] = cv_scores[1]
-                    new_row['cv_score3'] = cv_scores[2]
-                    new_row['cv_score4'] = cv_scores[3]
-                    new_row['cv_score5'] = cv_scores[4]
-                    new_row['mean_cv_score'] = cv_scores.mean()
-                    new_row['cv_score_std'] = cv_scores.std()
+    new_row = {
+        'n_estimators': n_estimators,
+        'learning_rate': learning_rate,
+        'max_depth': max_depth,
+        'subsample': subsample,
+        'colsample_bytree': colsample_bytree,
+        'cv_score1': cv_scores[0],
+        'cv_score2': cv_scores[1],
+        'cv_score3': cv_scores[2],
+        'cv_score4': cv_scores[3],
+        'cv_score5': cv_scores[4],
+        'mean_cv_score': cv_scores.mean(),
+        'cv_score_std': cv_scores.std()
+    }
 
-                    tuning = pd.concat([tuning,new_row], ignore_index = True)
+    tuning.loc[len(tuning)] = new_row
 
-                    count += 1
+    count += 1
 
-                    if count % 100 == 0:
-                        print(f'{count} simulations completed')
-                        tuning.to_csv('data/xgBoostTuning.csv')
+    if count % 5 == 0:
+        print(f'{count} simulations completed')
+        tuning.to_csv('data/xgBoostTuning.csv', index = False)
+
+print(tuning)
 
 '''
 model.fit(X_train, y_train, eval_set = [(X_test, y_test)], verbose = False)
